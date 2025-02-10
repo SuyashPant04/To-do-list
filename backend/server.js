5ac38afc724a6edc3afb1d9bdf1c2a1a000c0c61
SuyashPant04 <126603072+SuyashPant04@users.noreply.github.com>
2025-02-10 18:18:14 +0530

    project init

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/hello", (req, res) => {
+    res.send("hello bhai");
+  });
+
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
